c786c107342aa4715f882dc1feb8899b4420ddd6
brittanyabrahams2 <43973188+brittanyabrahams2@users.noreply.github.com>
2019-03-22 16:19:52 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myIntValue = 5/3;
+        float myFloatValue = 5f/3f;
+        double myDoubleValue = 5d/3d;
+
+        int pounds = 200;
+        double kilograms = 0.45359237;
+        double total = kilograms * pounds;
+        System.out.println(total);
+
+
+    }
+}
